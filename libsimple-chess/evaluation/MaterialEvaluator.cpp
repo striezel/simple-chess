36c7d720d44ce3bb4dfd7c06ee02d32fe5a1b649
Dirk Stolle <striezel-dev@web.de>
2024-02-03 01:34:32 +0100

    simplify branch condition in MaterialEvaluator::score()

--- libsimple-chess/evaluation/MaterialEvaluator.cpp
+++ libsimple-chess/evaluation/MaterialEvaluator.cpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of simple-chess.
-    Copyright (C) 2017, 2020, 2021  Dirk Stolle
+    Copyright (C) 2017, 2020, 2021, 2024  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -52,7 +52,10 @@ int MaterialEvaluator::score(const Board& board) const
   for (int i = static_cast<int>(Field::a1); i <= static_cast<int>(Field::h8); ++i)
   {
     const Piece elem = board.element(static_cast<Field>(i));
-    if ((elem.colour() == Colour::none) || (elem.piece() == PieceType::none))
+    // Checking for colour is enough, because PieceType::none will always occur
+    // together with Colour::none. And if it should not, then pieceValue() will
+    // return zero for PieceType::none.
+    if ((elem.colour() == Colour::none))
       continue;
 
     const int value(pieceValue(elem.piece()));
