f369a119105e4552274aadce2ecff77d22071365
Dirk Stolle <striezel-dev@web.de>
2024-02-03 01:16:09 +0100

    tests: slightly improve test coverage for MaterialEvaluator

--- tests/library/evaluation/MaterialEvaluator.cpp
+++ tests/library/evaluation/MaterialEvaluator.cpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the test suite for simple-chess.
-    Copyright (C) 2017, 2020  Dirk Stolle
+    Copyright (C) 2017, 2020, 2024  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -22,103 +22,110 @@
 #include "../../locate_catch.hpp"
 #include "../../../libsimple-chess/evaluation/MaterialEvaluator.hpp"
 
-TEST_CASE("MaterialEvaluator evaluates default start position")
-{
-  using namespace simplechess;
-  Board board;
-  REQUIRE( board.fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") );
-
-  MaterialEvaluator evaluator;
-  // Evaluation should be zero - both sides have the same material.
-  REQUIRE(
-      evaluator.score(board) == 0
-  );
-}
-
-TEST_CASE("MaterialEvaluator: different position with same material should not change score")
-{
-  using namespace simplechess;
-  Board boardOne;
-  REQUIRE( boardOne.fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") );
-
-  Board boardTwo;
-  REQUIRE( boardTwo.fromFEN("rnbqkbnr/pppp1ppp/4p3/8/8/4P3/PPPP1PPP/RNBQKBNR") );
-
-  // Scores should be equal - both boards have the same amount of material.
-  MaterialEvaluator evaluator;
-  REQUIRE(
-      evaluator.score(boardOne) == evaluator.score(boardTwo)
-  );
-}
-
-TEST_CASE("MaterialEvaluator: one missing rook for white")
-{
-  using namespace simplechess;
-  Board board;
-  REQUIRE( board.fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/1NBQKBNR") );
-
-  MaterialEvaluator evaluator;
-  // Evaluation should be less than zero - black has more.
-  REQUIRE(
-      evaluator.score(board) < 0
-  );
-  // Rook is five pawns, so value should be -500 centipawns.
-  REQUIRE(
-      evaluator.score(board) == -500
-  );
-}
-
-TEST_CASE("MaterialEvaluator: one missing rook for black")
-{
-  using namespace simplechess;
-  Board board;
-  REQUIRE( board.fromFEN("1nbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") );
-
-  MaterialEvaluator evaluator;
-  // Evaluation should be greater than zero - white has more material.
-  REQUIRE(
-      evaluator.score(board) > 0
-  );
-  // Rook is five pawns, so value should be 500 centipawns.
-  REQUIRE(
-      evaluator.score(board) == 500
-  );
-}
-
-TEST_CASE("MaterialEvaluator: one king to rule them all, white edition")
-{
-  using namespace simplechess;
-  Board board;
-  REQUIRE( board.fromFEN("rnbqqbnr/pppppppp/qqqqqqqq/qqqqqqqq/qqqqqqqq/qqqqqqqq/qqqqqqqq/4K3") );
-
-  MaterialEvaluator evaluator;
-  // Evaluation should be greater than zero, because black has no king and thus
-  // has lost.
-  REQUIRE(
-      evaluator.score(board) > 0
-  );
-}
-
-TEST_CASE("MaterialEvaluator: one king to rule them all, black edition")
-{
-  using namespace simplechess;
-  Board board;
-  REQUIRE( board.fromFEN("4k3/QQQQQQQQ/QQQQQQQQ/QQQQQQQQ/QQQQQQQQ/QQQQQQQQ/PPPPPPPP/RNBQQBNR") );
-
-  MaterialEvaluator evaluator;
-  // Evaluation should be less than zero, because white has no king and thus
-  // has lost.
-  REQUIRE(
-      evaluator.score(board) < 0
-  );
-}
-
-TEST_CASE("MaterialEvaluator: name")
+TEST_CASE("MaterialEvaluator")
 {
   using namespace simplechess;
 
-  MaterialEvaluator evaluator;
-  REQUIRE(
-      evaluator.name() == "MaterialEvaluator"
-  );
+  SECTION("score()")
+  {
+    MaterialEvaluator evaluator;
+
+    SECTION("evaluate default start position")
+    {
+      Board board;
+      REQUIRE( board.fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") );
+
+      // Evaluation should be zero - both sides have the same material.
+      REQUIRE(
+          evaluator.score(board) == 0
+      );
+    }
+
+    SECTION("different position with same material should not change score")
+    {
+      Board boardOne;
+      REQUIRE( boardOne.fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") );
+
+      Board boardTwo;
+      REQUIRE( boardTwo.fromFEN("rnbqkbnr/pppp1ppp/4p3/8/8/4P3/PPPP1PPP/RNBQKBNR") );
+
+      // Scores should be equal - both boards have the same amount of material.
+      REQUIRE(
+          evaluator.score(boardOne) == evaluator.score(boardTwo)
+      );
+    }
+
+    SECTION("one missing rook for white")
+    {
+      Board board;
+      REQUIRE( board.fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/1NBQKBNR") );
+
+      // Evaluation should be less than zero - black has more.
+      REQUIRE(
+          evaluator.score(board) < 0
+      );
+      // Rook is five pawns, so value should be -500 centipawns.
+      REQUIRE(
+          evaluator.score(board) == -500
+      );
+    }
+
+    SECTION("one missing rook for black")
+    {
+      Board board;
+      REQUIRE( board.fromFEN("1nbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") );
+
+      // Evaluation should be greater than zero - white has more material.
+      REQUIRE(
+          evaluator.score(board) > 0
+      );
+      // Rook is five pawns, so value should be 500 centipawns.
+      REQUIRE(
+          evaluator.score(board) == 500
+      );
+    }
+
+    SECTION("one king to rule them all, white edition")
+    {
+      Board board;
+      REQUIRE( board.fromFEN("rnbqqbnr/pppppppp/qqqqqqqq/qqqqqqqq/qqqqqqqq/qqqqqqqq/qqqqqqqq/4K3") );
+
+      // Evaluation should be greater than zero, because black has no king and
+      // thus has lost.
+      REQUIRE(
+          evaluator.score(board) > 0
+      );
+    }
+
+    SECTION("one king to rule them all, black edition")
+    {
+      Board board;
+      REQUIRE( board.fromFEN("4k3/QQQQQQQQ/QQQQQQQQ/QQQQQQQQ/QQQQQQQQ/QQQQQQQQ/PPPPPPPP/RNBQQBNR") );
+
+      // Evaluation should be less than zero, because white has no king and
+      // thus has lost.
+      REQUIRE(
+          evaluator.score(board) < 0
+      );
+    }
+  }
+
+  SECTION("name")
+  {
+    MaterialEvaluator evaluator;
+    REQUIRE(
+        evaluator.name() == "MaterialEvaluator"
+    );
+  }
+
+  SECTION("pieceValue")
+  {
+    REQUIRE( MaterialEvaluator::pieceValue(PieceType::none) == 0 );
+    REQUIRE( MaterialEvaluator::pieceValue(PieceType::pawn) == 100 );
+    REQUIRE( MaterialEvaluator::pieceValue(PieceType::bishop) == 300 );
+    REQUIRE( MaterialEvaluator::pieceValue(PieceType::knight) == 300 );
+    REQUIRE( MaterialEvaluator::pieceValue(PieceType::rook) == 500 );
+    REQUIRE( MaterialEvaluator::pieceValue(PieceType::queen) == 900 );
+    REQUIRE( MaterialEvaluator::pieceValue(PieceType::king) > 900 );
+  }
 }
